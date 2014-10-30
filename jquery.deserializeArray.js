aa85f321537575829da8d3a5dd8ce3a6756fd7d9
Mikael Karon <mikael@karon.se>
2014-10-30 10:35:52 +0800

    Initial commit

---
+++ jquery.deserializeArray.js
@@ -0,0 +1,27 @@
+/**
+ * @license MIT http://mu-lib.mit-license.org/
+ */
+(function (factory) {
+	if (typeof exports === "object") {
+		factory(require("jquery"), require("mu-jquery-deserialize-object"));
+	}
+	else if (typeof define === "function" && define.amd) {
+		define([ "jquery", "mu-jquery-deserialize-object" ], factory);
+	}
+	else {
+		factory(window.jQuery)
+	}
+}(function ($) {
+	var NAME = "name";
+	var VALUE = "value";
+
+	$.fn.deserializeArray = function (array) {
+		var object = {};
+
+		$.each(array || false, function (result, field) {
+			object[field[NAME]] = field[VALUE];
+		});
+
+		return this.deserializeObject(object);
+	}
+}));
